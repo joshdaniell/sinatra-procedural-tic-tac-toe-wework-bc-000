0f6f8b20aad609807c951ed17e8c105497b252a9
Josh Daniell <josh.daniell@wework.com>
2018-05-19 23:26:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class Application < Sinatra::Base
+  
+  get '/' do
+    @game = TicTacToe.new
+    erb :index
+  end
+  
+  post '/' do
+    erb :index
+  end
+  
 end
